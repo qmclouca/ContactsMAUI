43e76f03bf4595beb944cabc71254020141ee58a
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2024-05-20 20:45:31 -0300

    Refatorando para arquitetura em camadas

---
+++ Contacts.UseCases/PluginInterfaces/IContactRepository.cs
@@ -0,0 +1,12 @@
+﻿using Contact = Contacts.CoreBusiness.Contact;
+namespace Contacts.UseCases.PluginInterfaces
+{
+    public interface IContactRepository
+    {
+        Task<List<Contact>> SearchContacts(string filterText);
+        Task<List<Contact>> GetAllContacts();
+        Task AddContact(Contact contact);
+        Task UpdateContact(Contact contact);
+        Task RemoveContact(Contact contact);
+    }
+}
---
+++ Contacts.UseCases/ViewContactsUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Contacts.UseCases.PluginInterfaces;
+using Contact = Contacts.CoreBusiness.Contact;
+namespace Contacts.UseCases
+{
+    public class ViewContactsUseCase
+    {
+        private readonly IContactRepository _contactRepository;
+        public ViewContactsUseCase(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;             
+        }
+        public async Task<List<Contact>> ExecuteAsync(string filterText)
+        {
+            return await _contactRepository.SearchContacts(filterText);
+        }
+    }
+}
