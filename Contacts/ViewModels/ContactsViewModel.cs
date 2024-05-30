82239c93ba893a279916111f306006138761d101
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2024-05-30 08:02:24 -0300

    MVVM with clean code

---
+++ Contacts/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contacts.UseCases.Interfaces;
+using Contact = Contacts.CoreBusiness.Contact;
+
+namespace Contacts.ViewModels
+{
+    public class ContactsViewModel
+    {
+        private readonly IViewContactsUseCase _viewContactsUseCase;
+
+        public ObservableCollection<Contact> Contacts { get; set; }
+        public ContactsViewModel(IViewContactsUseCase viewContactsUseCase)
+        {
+            _viewContactsUseCase = viewContactsUseCase;
+            this.Contacts = new ObservableCollection<Contact>();
+        }
+
+        public async Task LoadContactsAsync()
+        {
+            this.Contacts.Clear();
+
+            var contacts = await _viewContactsUseCase.ExecuteAsync(null);
+            if (contacts != null && contacts.Count > 0)
+            {
+                foreach (var contact in contacts)
+                {
+                    Contacts.Add(contact);
+                }
+            }
+        }
+    }
+}
